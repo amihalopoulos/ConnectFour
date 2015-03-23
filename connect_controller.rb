de9d20354e52f0a9c8d35c393360111102a38553
amihalopoulos & Greg Cohen <pair+amihalopoulos.gregmilescohen@devbootcamp.com>
2015-03-23 15:05:40 -0400

    Initial test to count upwards

--- connect_controller.rb
+++ connect_controller.rb
@@ -4,19 +4,7 @@ player = ["X", "O"]
 
 board = Board.new(player)
 
-
-until false #board.winner
-  player.each do |player|
-    puts board
-    # puts "Player #{player}: What's Your Move?"
-    # position = gets.chomp.to_i
-    get_position
-    board.place(player, position)
-    board.clear_screen
-  end
-end
-
-def get_position
+def get_position(player)
   puts "Player #{player}: What's Your Move?"
   position = gets.chomp.to_i
   if position > 6 || position < 0
@@ -25,5 +13,18 @@ def get_position
   end
   position
 end
+
+until false #board.winner
+  player.each do |player|
+    puts board
+    # puts "Player #{player}: What's Your Move?"
+    # position = gets.chomp.to_i
+    position = get_position(player)
+    board.place(player, position)
+    board.clear_screen
+  end
+end
+
+
 
 
--- connect_four.rb
+++ connect_four.rb
@@ -5,24 +5,63 @@
 #  -take arguments of player, column (X, 3)
 
 class Board
-
+    attr_reader :board
   def initialize(player)
     @board = Array.new(7) {[" ", " ", " ", " ", " ", " ", " "]}
-    @col_board = @board.transpose
     @player = player
   end
 
-  def place(player, position, x = -1)
-    return "Column Filled!" if x == -8
+  def place(player, position, x = 0)
+    return "Column Filled!" if x == 7
     if @board[position][x] == ' '
       @board[position][x].replace(player)
     else
-      place(player, position, x -= 1)
+      place(player, position, x += 1)
+    end
+    y = @board[position].index(player)
+    x = position
+    #call method to check connect 4
+  end
+
+  def check_connect_four
+    check_row || check_col || check_diag
+  end
+
+  def check_up_down(player, x, y, count = 0) #can we pass cell(x,y)
+   return true if count == 4
+   if cell(x, y) == player
+    #count += 1
+    check_up_down(player, x, y + 1, count +=1)
+  end
+  end
+
+  def check(player, x, y)
+    i = 1
+    until i > 3
+      cell(x + i, y) #==> 'x' or ' '
+      cell(x - i, y)
+      cell(x, y - i)
+      cell(x, y + i)
+      cell(x + i, y + i)
+      cell(x - i, y + i)
+      cell(x + i, y - i)
+      cell(x - i, y - i)
+      i += 1
     end
   end
 
+  def cell(x, y)
+    if x < 0 || x > 7 || y < 0 || y > 6
+      return nil
+    else
+      @board[x][y]
+    end
+  end
+
+
+
   def to_s
-    @board.transpose.each do |column|
+    @board.transpose.reverse.each do |column|
       puts "| " + column.join(' ') + " |"
     end
       puts "-----------------"
@@ -40,15 +79,18 @@ class Board
 end
 
   game1 = Board.new(players = ["x", "y"])
-  # game1.place("x", 3)
-# game1.place("O", 3)
-# game1.place("O", 3)
-# game1.place("O", 3)
-# game1.place("O", 3)
+   game1.place("x", 3)
+ game1.place("O", 3)
+ game1.place("O", 3)
+ game1.place("O", 3)
+ game1.place("O", 3)
 # game1.place("O", 3)
 # game1.place("O", 3)
 # game1.place("X", 3)
- #game1.to_s
+ game1.to_s
+ p game1.board
+ p game1.check_up_down("O", 3, 1)
+
 
 
 #>player 1 whats yuour move? input =gets.chomp
